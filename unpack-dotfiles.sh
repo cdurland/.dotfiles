ff659bf1d2f5209c1edf28e2368ef69c855620d8
cdurland <chip.durland@nccgroup.trust>
2016-02-23 11:05:52 -0500

    update

--- unpack-dotfiles.sh
+++ unpack-dotfiles.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-# Install SPF13
-echo "[+] Installing SPF13"
-curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
-
 echo ""
 echo "Checking for ZSH"
 which zsh
@@ -13,12 +8,17 @@ which zsh
 if [ $? > 0 ]
 then
     echo "ZSH not found!"
+    echo "Please install ZSH first"
+    exit 1
 else
     echo "ZSH found!"
     echo "[+] Installing Oh-my-zsh"
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+# Install SPF13
+echo "[+] Installing SPF13"
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 
 echo "[+] Checking for .dotfiles directory"
 if [ ! -d ~/.dotfiles ]
